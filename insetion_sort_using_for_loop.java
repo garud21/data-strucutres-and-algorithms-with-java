fdce872fe7655e70ebcba791498e5a38a2d91648
garud21 <87640570+garud21@users.noreply.github.com>
2023-06-04 13:42:26 +0530

    Update and rename insetion_sort.java to insetion_sort_using_for_loop.java

--- insetion_sort_using_for_loop.java
+++ insetion_sort_using_for_loop.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class insetion_sort {
+public class insertion_sort_using_for_loop {
     public static void main(String[] args) {
         int[] arr = new int[5];
         Scanner sc = new Scanner(System.in);
